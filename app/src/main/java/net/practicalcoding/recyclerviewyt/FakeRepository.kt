91b560b7765e6f504071e2edc28144cf7cc60ec4
Jason <Jason@practicalcoding.net>
2022-12-08 14:44:06 -0800

    Initial Commit

---
+++ app/src/main/java/net/practicalcoding/recyclerviewyt/FakeRepository.kt
@@ -0,0 +1,95 @@
+package net.practicalcoding.recyclerviewyt
+
+object FakeRepository {
+    val moleculeName = mapOf(
+        MoleculeId.ALANINE to "Alanine",
+        MoleculeId.ARGININE to "Arginine",
+        MoleculeId.ASPARAGINE to "Asparagine",
+        MoleculeId.ASPARTIC_ACID to "Aspartic Acid",
+        MoleculeId.CYSTEINE to "Cysteine",
+        MoleculeId.GLUTAMIC_ACID to "Glutamic Acid",
+        MoleculeId.GLUTAMINE to "Glutamine",
+        MoleculeId.GLYCINE to "Glycine",
+        MoleculeId.HISTIDINE to "Histidine",
+        MoleculeId.ISOLEUCINE to "Isoleucine",
+        MoleculeId.LEUCINE to "Leucine",
+        MoleculeId.LYSINE to "Lysine",
+        MoleculeId.METHIONINE to "Methionine",
+        MoleculeId.PHENYLALANINE to "Phenylalanine",
+        MoleculeId.PROLINE to "Proline",
+        MoleculeId.SERINE to "Serine",
+        MoleculeId.THREONINE to "Threonine",
+        MoleculeId.TRYPTOPHAN to "Tryptophan",
+        MoleculeId.TYROSINE to "Tyrosine",
+        MoleculeId.VALINE to "Valine"
+    )
+
+    val molecule3LetterAbbr = mapOf(
+        MoleculeId.ALANINE to "Ala",
+        MoleculeId.ARGININE to "Arg",
+        MoleculeId.ASPARAGINE to "Asn ",
+        MoleculeId.ASPARTIC_ACID to "As",
+        MoleculeId.CYSTEINE to "Cyc",
+        MoleculeId.GLUTAMIC_ACID to "Glu",
+        MoleculeId.GLUTAMINE to "Gln",
+        MoleculeId.GLYCINE to "Gly",
+        MoleculeId.HISTIDINE to "His",
+        MoleculeId.ISOLEUCINE to "Ile",
+        MoleculeId.LEUCINE to "Leu",
+        MoleculeId.LYSINE to "Lys",
+        MoleculeId.METHIONINE to "Met",
+        MoleculeId.PHENYLALANINE to "Phe",
+        MoleculeId.PROLINE to "Pro",
+        MoleculeId.SERINE to "Ser",
+        MoleculeId.THREONINE to "Thr",
+        MoleculeId.TRYPTOPHAN to "Trp",
+        MoleculeId.TYROSINE to "Tyr",
+        MoleculeId.VALINE to "Val"
+    )
+
+    val molecule1LetterAbbr = mapOf(
+        MoleculeId.ALANINE to "A",
+        MoleculeId.ARGININE to "R",
+        MoleculeId.ASPARAGINE to "N",
+        MoleculeId.ASPARTIC_ACID to "D",
+        MoleculeId.CYSTEINE to "C",
+        MoleculeId.GLUTAMIC_ACID to "E",
+        MoleculeId.GLUTAMINE to "Q",
+        MoleculeId.GLYCINE to "G",
+        MoleculeId.HISTIDINE to "H",
+        MoleculeId.ISOLEUCINE to "I",
+        MoleculeId.LEUCINE to "L",
+        MoleculeId.LYSINE to "K",
+        MoleculeId.METHIONINE to "M",
+        MoleculeId.PHENYLALANINE to "F",
+        MoleculeId.PROLINE to "P",
+        MoleculeId.SERINE to "S",
+        MoleculeId.THREONINE to "T",
+        MoleculeId.TRYPTOPHAN to "W",
+        MoleculeId.TYROSINE to "Y",
+        MoleculeId.VALINE to "V"
+    )
+
+    val structures = mapOf(
+        MoleculeId.ALANINE to R.drawable.alanine,
+        MoleculeId.ARGININE to R.drawable.arginine,
+        MoleculeId.ASPARAGINE to R.drawable.asparagine,
+        MoleculeId.ASPARTIC_ACID to R.drawable.aspartic_acid,
+        MoleculeId.CYSTEINE to R.drawable.cystine,
+        MoleculeId.GLUTAMIC_ACID to R.drawable.glutamic_acid,
+        MoleculeId.GLUTAMINE to R.drawable.glutamine,
+        MoleculeId.GLYCINE to R.drawable.glycine,
+        MoleculeId.HISTIDINE to R.drawable.histidine,
+        MoleculeId.ISOLEUCINE to R.drawable.isoleucine,
+        MoleculeId.LEUCINE to R.drawable.leucine,
+        MoleculeId.LYSINE to R.drawable.lysine,
+        MoleculeId.METHIONINE to R.drawable.methionine,
+        MoleculeId.PHENYLALANINE to R.drawable.phenylalanine,
+        MoleculeId.PROLINE to R.drawable.proline,
+        MoleculeId.SERINE to R.drawable.serine,
+        MoleculeId.THREONINE to R.drawable.threonine,
+        MoleculeId.TRYPTOPHAN to R.drawable.tryptophan,
+        MoleculeId.TYROSINE to R.drawable.tyrosine,
+        MoleculeId.VALINE to R.drawable.valine
+    )
+}
